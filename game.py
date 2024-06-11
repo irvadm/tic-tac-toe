44811a4ee2057818990d97ea55ef22e330bd9839
irvadm <amiam89@gmail.com>
2024-06-11 15:26:46 -0400

    Create and draw board

---
+++ game.py
@@ -0,0 +1,15 @@
+def draw_board(board):
+    print(f'{board[0]}|{board[1]}|{board[2]}')
+    print('-+-+-')
+    print(f'{board[3]}|{board[4]}|{board[5]}')
+    print('-+-+-')
+    print(f'{board[6]}|{board[7]}|{board[8]}')
+
+
+def main():
+    board = [' '] * 9
+    draw_board(board)
+
+
+if __name__ == '__main__':
+    main()
