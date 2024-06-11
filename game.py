5378347aafd2e3e57443d9253ce158a57555e95d
irvadm <amiam89@gmail.com>
2024-06-11 15:53:43 -0400

    Randomly choose who gets to go first

--- game.py
+++ game.py
@@ -1,3 +1,6 @@
+import random
+
+
 def draw_board(board):
     print(f'{board[0]}|{board[1]}|{board[2]}')
     print('-+-+-')
@@ -21,9 +24,23 @@ def ask_player_letter():
                 return player, 'X'
 
 
+def who_goes_first():
+    """Randomly decide who goes first."""
+    random_number = random.randint(0, 1)
+    if random_number == 0:
+        return 'X'
+    else:
+        return 'O'
+
+
 def main():
     # Get player and computer letter
     player, computer = ask_player_letter()
+    turn = who_goes_first()
+    if turn == computer:
+        print('The computer will go first.')
+    else:
+        print('You will go first.')
     board = [' '] * 9
     draw_board(board)
 
